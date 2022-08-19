f7e69307ca24794b7c1354a8bc761edd97eec367
Egor Nikolaev <egornikolaev97@yandex.ru>
2022-08-19 12:03:20 +0500

    fix: review 1. add constant to app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const NOT_FOUND_STATUS = require('./utils/status');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -22,7 +23,7 @@ app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
 app.use((req, res) => {
-  res.status(404).send({ message: 'Такой страницы не существует' });
+  res.status(NOT_FOUND_STATUS).send({ message: 'Такой страницы не существует' });
 });
 
 app.listen(PORT, () => {
