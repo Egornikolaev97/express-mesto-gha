7608e6a961ebc8475cfc5b3c781838a9820489a5
Egor Nikolaev <egornikolaev97@yandex.ru>
2022-08-19 12:08:37 +0500

    fix: test1 after review1

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const NOT_FOUND_STATUS = require('./utils/status');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -23,7 +22,7 @@ app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
 app.use((req, res) => {
-  res.status(NOT_FOUND_STATUS).send({ message: 'Такой страницы не существует' });
+  res.status(404).send({ message: 'Такой страницы не существует' });
 });
 
 app.listen(PORT, () => {
