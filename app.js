6e4135576235baf3abca7112ce9e186897372e0c
Egor Nikolaev <egornikolaev97@yandex.ru>
2022-08-27 10:55:31 +0500

    add: auth.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const handleError = require('./midlewares/handleError');
+const { createUser, login } = require('./controllers/users');
+const auth = require('./midlewares/auth');
 const { NOT_FOUND_STATUS } = require('./utils/status');
 
 const { PORT = 3000 } = process.env;
@@ -17,15 +20,45 @@ app.use((req, res, next) => {
   next();
 });
 
+app.post(
+  '/signup',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+      avatar: Joi.string().regex(/^http(s)?:\/\/((www.)?([\w-]+\.)+\/?)\S*$/),
+    }),
+  }),
+  createUser,
+);
+app.post(
+  '/signin',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+    }),
+  }),
+  login,
+);
+app.use(auth);
+
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
+app.use(handleError);
+
 app.use((req, res) => {
-  res.status(NOT_FOUND_STATUS).send({ message: 'Такой страницы не существует' });
+  res
+    .status(NOT_FOUND_STATUS)
+    .send({ message: 'Такой страницы не существует' });
 });
 
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log('hello world');
 });
--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { Joi, celebrate } = require('celebrate');
 const {
   getUsers,
   getUser,
@@ -8,9 +9,39 @@ const {
 } = require('../controllers/users');
 
 router.get('/', getUsers);
-router.get('/:userId', getUser);
 router.post('/', createUser);
-router.patch('/me', updateUserInfo);
-router.patch('/me/avatar', updateUserAvatar);
+
+router.get(
+  '/:userId',
+  celebrate({
+    params: Joi.object().keys({
+      userId: Joi.string().length(24).hex(),
+    }),
+  }),
+  getUser,
+);
+
+router.patch(
+  '/me',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30).required(),
+      about: Joi.string().min(2).max(30).required(),
+    }),
+  }),
+  updateUserInfo,
+);
+
+router.patch(
+  '/me/avatar',
+  celebrate({
+    body: Joi.object().keys({
+      avatar: Joi.string()
+        .required()
+        .regex(/^http(s)?:\/\/((www.)?([\w-]+\.)+\/?)\S*$/),
+    }),
+  }),
+  updateUserAvatar,
+);
 
 module.exports = router;
